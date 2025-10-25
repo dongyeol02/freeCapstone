b8f4471cb0611243436666a6f7c542d4764f0212
dongyeol02 <sprent7@gmail.com>
2025-10-26 01:15:00 +0900

    feat: router 설정

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,6 @@
+const Navbar = () => {
+  const bgBlack = "bg-gradient-to-b from-[#1A1625] via-[#1E1B4B] to-[#2D1B4E]";
+  return <nav className={`w-full h-37.5 ${bgBlack} text-white`}>Navbar..</nav>;
+};
+
+export default Navbar;
